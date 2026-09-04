079966b11e15d76474f74eac696a072cc444b11f
pouniq <abdollahpora@gmail.com>
2026-09-04 15:00:38 +0330

    added db files

---
+++ MySQL/tamrin_5_SQL/db/sql/categories_202609041457.sql
@@ -0,0 +1,26 @@
+INSERT INTO KohanNegar.categories (id,title,parent_id) VALUES
+	 (7,'Classical Persian Poetry',1),
+	 (8,'Contemporary Poetry',1),
+	 (9,'Translated World Poetry',1),
+	 (10,'Illustrated Poetry Editions',1),
+	 (11,'Ancient Persia',2),
+	 (12,'Islamic Era History',2),
+	 (13,'Modern Iranian History',2),
+	 (14,'World History',2),
+	 (15,'Local/Regional History',2),
+	 (16,'Miniature Reproductions',3);
+INSERT INTO KohanNegar.categories (id,title,parent_id) VALUES
+	 (17,'Oil Painting Books',3),
+	 (18,'Watercolor Collections',3),
+	 (19,'Calligraphy Art',3),
+	 (20,'Black & White Photography',4),
+	 (21,'Landscape Photography',4),
+	 (22,'Portrait Photography',4),
+	 (23,'Documentary Photography',4),
+	 (24,'Single-Artist Retrospectives',5),
+	 (25,'Limited Edition Artist Sets',5),
+	 (26,'Signed Artist Collections',5);
+INSERT INTO KohanNegar.categories (id,title,parent_id) VALUES
+	 (27,'Antique Reprints',6),
+	 (28,'Facsimile Editions',6),
+	 (29,'Numbered Collector Editions',6);
---
+++ MySQL/tamrin_5_SQL/db/sql/roles_202609041457.sql
@@ -0,0 +1,4 @@
+INSERT INTO KohanNegar.roles (id,title) VALUES
+	 (1,'admin'),
+	 (2,'staff'),
+	 (3,'customer');
